48a20d38f0a221a04c2fb01b73f85b5778bfddcb
gkbrk <webdosusb@gmail.com>
2014-10-08 19:32:20 +0300

    Made the heartbeat immutable.

--- src/heartbeat.rs
+++ src/heartbeat.rs
@@ -12,25 +12,25 @@ pub struct Heartbeat{
 }
 
 impl Heartbeat{
-    pub fn new(&mut self, config: Configuration) -> Heartbeat{
+    pub fn new(config: Configuration) -> Heartbeat{
         return Heartbeat{
             config: config
         };
     }
     
-    pub fn send_heartbeat(&mut self){
+    pub fn send_heartbeat(self){
         let response = http::handle().get(format!("https://minecraft.net/heartbeat.jsp?port={:u}&max={:u}&name={:s}&public={:s}&version=7&salt={:s}&users=0", self.config.port, self.config.max_players, self.config.server_name.as_slice(), self.config.is_public.as_slice(), self.config.salt.as_slice())).exec().unwrap();
     }
     
-    pub fn loop_blocking(&mut self){
+    pub fn loop_blocking(self){
         loop{
             self.send_heartbeat();
             timer::sleep(Duration::seconds(self.config.heartbeat_interval));
         }
     }
     
-    pub fn spawn_task(&mut self){
-        let mut clone = self.clone();
+    pub fn spawn_task(self){
+        let clone = self.clone();
         spawn(proc() {
             clone.loop_blocking();
         });
