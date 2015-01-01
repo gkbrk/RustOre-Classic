d9575f1688312088ddbadc25cb9f3c7312f71258
Gökberk <webdosusb@gmail.com>
2015-01-01 17:49:10 +0200

    Moved default configuration to config.rs

--- src/config.rs
+++ src/config.rs
@@ -10,3 +10,19 @@ pub struct Configuration{
     pub salt: String,
     pub heartbeat_interval: i64
 }
+
+impl Configuration{
+    fn get_default_config() -> Configuration{
+        return Configuration{
+            address: "0.0.0.0".to_string(),
+            port: 25565,
+            max_players: 20,
+            server_name: "RustOreBeta".to_string(),
+            server_motd: "A Minecraft classic server written in Rust".to_string(),
+            is_public: "True".to_string(),
+            online_mode: false,
+            salt: task_rng().gen_ascii_chars().take(16).collect(),
+            heartbeat_interval: 45
+        };
+    }
+}
